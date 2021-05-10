c332a828ab50b23386b0e8a334a65aa28eb279bc
dashartava <54775974+dashartava@users.noreply.github.com>
2021-05-10 08:41:56 +0200

    Babatunde

---
+++ index.js
@@ -0,0 +1,6 @@
+function gegilou() {
+    document.getElementById("gegi").src = "tvit-gegi.png";
+    document.getElementById("Button").style.display = "none";
+    document.getElementById("gegi").style.width = "25%";
+};
+
